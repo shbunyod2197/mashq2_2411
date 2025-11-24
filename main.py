aee489404efe024566556bcca9ab3c0ca45f9f8f
shbunyod2197 <shbunyod2197@gmail.com>
2025-11-24 16:05:07 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+1-m
+m = input("Matn kirit: ")
+find  = input("Qidirilayotgan matn kirit: ")
+
+if find in m:
+    print("Topildi")
+else:
+    print("Topilmadi")
+# 02-m
+n = input("Matn kirit: ")
+find = input("Qidirilayotgan matn kirit: ")
+if find in n :
+    print("Topildi")
+else:
+    print("Topilmadi")
+# 03-m
+b = input("Matn kirit: ")
+find = input("Qidirilayotgan matn kirit: ")
+if find in b:
+    print("So'roq bor")
+else:
+    print("So'roq yo'q")
+# 04-m
+v = input("kontakt kirit: ")
+find = input("Qidirilayotgan qisimni kirit: ")
+
+if v in find:
+    print("O'zbekisto")
+else:
+    print("Boshqa davlat")
+# 05-m
+c = input("Email kirit: ")
+find = input("Qidirilayotgan belgini kirit: ")
+
+if c in find:
+    print("@ bor")
+else:
+    print("@ yo'q")
